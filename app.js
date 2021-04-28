f25ceb6439d28d9ae337f343bfef1458fea97397
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2021-04-28 15:51:48 +0100

    quizGame

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const questions = JSON.parse(fs.readFileSync('./data/questions.json'));
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api/v1/question/:id', (req, res, next) => {
+  if (req.params.id > questions[questions.length - 1].id) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No question with this id',
+    });
+  }
+  next();
+});
+
+// ROUTE HANDLERS
+app.get('/api/v1/question/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const question = questions.find((q) => q.id === id);
+  console.log(question);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      id,
+      question: question.question,
+      answers: question.answers,
+    },
+  });
+});
+
+app.post('/api/v1/question/:id', (req, res) => {
+  const questionID = req.params.id * 1;
+  const answerID = req.body.id;
+  const question = questions.find((q) => q.id === questionID);
+  const correct = question.correct === answerID ? 'correct' : 'incorrect';
+  const correctAnswer = question.answers.find(
+    (el) => el.id === question.correct
+  );
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      answer: correct,
+      correctAnswer: correctAnswer.text,
+    },
+  });
+});
+
+// SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log('App listening on port 3000!');
+});
